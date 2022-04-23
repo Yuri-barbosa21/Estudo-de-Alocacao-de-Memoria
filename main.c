b1eb759079bc877e88171a256cef05dc84ccd187
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-23 20:09:52 +0000

    Alocacao dinamica de memoria com Calloc

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main(void){
 
     // ----------------- ALOCAÇÃO DINAMICA DE MEMORIA COM MALLOC ------------------
+    /*
     int *x;
 
     x = malloc(sizeof(int));
@@ -17,6 +18,27 @@ int main(void){
     else{
         printf("Erro ao alocar memoria!\n");
     }
+    */
+    //_____________________________________________________________________________
+
+
+
+    // ----------------- ALOCAÇÃO DINAMICA DE MEMORIA COM CALLOC ------------------
+    
+    char *x;
+
+    x = calloc(1, sizeof(char));
+
+    if(x){
+        printf("Memoria alocada com sucesso!\n");
+        printf("x: %c\n", *x);
+        *x = 'C';
+        printf("x: %c\n", *x);
+    }
+    else{
+        printf("Erro ao alocar memoria!\n");
+    }
+    
     //_____________________________________________________________________________
     
     return 0;
