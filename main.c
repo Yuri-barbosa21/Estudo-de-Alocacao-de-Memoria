c4674dd669d8e7fe9bbfc982d15beacb09e2961a
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-23 21:01:35 +0000

    Preencher matriz dinamicamente

--- main.c
+++ main.c
@@ -84,6 +84,7 @@ int main(void){
 
 
     //-------------------------------- FUNÇÃO FREE --------------------------------
+    /*
     int i, tam, r, *vet;
 
     printf("Digite o tamanho do vetor: ");
@@ -118,7 +119,38 @@ int main(void){
     }
 
     free(vet);
+
+    */
     //_____________________________________________________________________________
+
+
+
+
+    //------------------------ PREENCHER MATRIZ DINAMICAMENTE ----------------------
+    int **mat, i, j;
+
+    mat = malloc(4 * sizeof(int));
+
+    for(i = 0; i < 4; i++){
+        mat[i] = malloc(3 * sizeof(int));
+    }
+
+    srand(time(NULL));
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 3; j++){
+            mat[i][j] = rand() % 100;
+        }
+    }
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+    //_____________________________________________________________________________
+
     
     return 0;
 }
