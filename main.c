2ef700057699990118429b958d923d43fe868878
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-23 21:07:12 +0000

    Preencher matriz com aritmetica de ponteiros

--- main.c
+++ main.c
@@ -127,6 +127,7 @@ int main(void){
 
 
     //------------------------ PREENCHER MATRIZ DINAMICAMENTE ----------------------
+    /*
     int **mat, i, j;
 
     mat = malloc(4 * sizeof(int));
@@ -149,8 +150,35 @@ int main(void){
         }
         printf("\n");
     }
+    */
     //_____________________________________________________________________________
 
-    
+
+
+
+    //---------------- PREENCHER MATRIZ COM ARITMETICA DE PONTEIROS ----------------
+    int **mat, i, j;
+
+    mat = malloc(4 * sizeof(int));
+
+    for(i = 0; i < 4; i++){
+        mat[i] = malloc(3 * sizeof(int));
+    }
+
+    srand(time(NULL));
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 3; j++){
+            *(*(mat + i) + j) = rand() % 100;
+        }
+    }
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ", *(*(mat + i) + j));
+        }
+        printf("\n");
+    }
+    //_____________________________________________________________________________
     return 0;
 }
