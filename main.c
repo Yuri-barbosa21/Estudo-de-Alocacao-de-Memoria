35db75a07d43eec35930e96cc93455d7943b1249
Yuri <62626768+Yuri-barbosa21@users.noreply.github.com>
2022-04-24 19:49:26 +0000

    Liberar memoria de uma matriz dinamica

--- main.c
+++ main.c
@@ -157,6 +157,7 @@ int main(void){
 
 
     //---------------- PREENCHER MATRIZ COM ARITMETICA DE PONTEIROS ----------------
+    /*
     int **mat, i, j;
 
     mat = malloc(4 * sizeof(int));
@@ -179,6 +180,42 @@ int main(void){
         }
         printf("\n");
     }
+    */
     //_____________________________________________________________________________
+
+
+
+    //------------------- LIBERAR MEMORIA DE UMA MATRIZ DINAMICA ------------------
+    
+    int **mat, i, j;
+
+    mat = malloc(4 * sizeof(int));
+
+    for(i = 0; i < 4; i++){
+        mat[i] = malloc(3 * sizeof(int));
+    }
+
+    srand(time(NULL));
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 3; j++){
+            *(*(mat + i) + j) = rand() % 100;
+        }
+    }
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ", *(*(mat + i) + j));
+        }
+        printf("\n");
+    }
+
+    for(i = 0; i < 5; i++){
+        free(mat[i]);
+    }
+    free(mat);
+    
+    //_____________________________________________________________________________
+    
     return 0;
 }
